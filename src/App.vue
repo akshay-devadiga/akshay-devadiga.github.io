2d7fe161fa211e3a259a556535fc383f6be11512
akshay-devadiga <akshaydevadiga398@gmail.com>
2021-04-05 22:27:06 +0530

    Add background

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-<div class=''>
-    {{'Work in progress'}}
-    <img src="https://image.flaticon.com/icons/png/512/83/83114.png" width="150" height="160">
+<div class='root'>
+    <img class="vertical-center" src="https://image.flaticon.com/icons/png/512/83/83114.png" width="80" height="80">
+    <p> {{'Work in progress'}} </p>
 </div>
 </template>
 <style scoped>
-  .title{
-      background:yellow;
-  }
-  body{
-      font-family: 'Slabo 13px', serif;
-  }
+  .root{
+    height: 95vh;
+    background: #FFE66D;
+    width: 100%;
+    padding:0.1%;
+    margin:0;
+    text-align: center;
+    font-family: 'Slabo 13px', serif !important;
+    font-size: 30px;
+    font-weight: bold;
+   }
+ .vertical-center {
+    margin-top:15%;
+}
 </style>
