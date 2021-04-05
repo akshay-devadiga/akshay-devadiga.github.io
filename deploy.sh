2ef7e7ccf725bad1ac5fee223406ebacebee29bc
akshay-devadiga <akshaydevadiga398@gmail.com>
2021-04-05 21:38:10 +0530

    fix issue with deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 cd dist
 echo "-----------------------------  Inside dist folder ----------------------------- "
 git fetch --all
-git checkout deploy-dist
+git checkout dist-deploy
 echo "-----------------------------  Checked out remote branch ----------------------------- "
-git pull origin deploy-dist
+
 echo "-----------------------------  Pulling the latest code ----------------------------- "
 git add . 
 echo "-----------------------------  Adding local files ----------------------------- "
 git commit -m "New deploy"
 echo "----------------------------- Commiting  ----------------------------- "
-git push -u origin deploy-dist
+git push -u origin dist-deploy
 echo "----------------------------- Updated -----------------------------"
 cd ..
 git checkout main
+echo "----------------------------- Completed -----------------------------"
