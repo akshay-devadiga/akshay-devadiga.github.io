af9cb3891ea8c43c681047c513b325ef4272885b
Akshay <39487594+akshay-devadiga@users.noreply.github.com>
2023-03-10 08:45:20 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 git push -f https://github.com/akshay-devadiga/akshay-devadiga.github.io.git main:gh-pages
 cd -
