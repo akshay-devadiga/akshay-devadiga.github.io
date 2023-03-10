725544c5581937930c2a47261598b26c1066626a
Akshay <39487594+akshay-devadiga@users.noreply.github.com>
2023-03-10 08:32:26 +0000

    Upgrade deploy script

--- deploy.sh
+++ deploy.sh
@@ -18,12 +18,13 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
-# build
-npm run build
+# build Make sure you build the app
+#npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+git add .
 git commit -m 'deploy'
 git push -f https://github.com/akshay-devadiga/akshay-devadiga.github.io.git main:gh-pages
 cd -
