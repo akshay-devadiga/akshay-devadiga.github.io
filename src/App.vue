e7f2a751160a93efc1d2174d043bd7c75f7b2b9c
add11e2 <akshaydevadiga4@gmail.com>
2023-03-28 23:27:01 +0530

    Update navbar

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
   <v-app class="pa-0">
-    <v-card  class="pa-0" min-width="100%">
+    <v-card class="pa-0" min-width="100%">
       <v-layout full-height>
-      <v-navigation-drawer permanent color="#62ADF4">
-      
-        <sidebar />
-      </v-navigation-drawer>
-      <v-main >
-        <v-container class="fill-height" fluid>
-          <v-row>
-          <v-col cols="12">
-              <v-row class="justify-center align-center">
-                <v-col cols="12">
-                  <AboutView style="width:90vw" />
-                </v-col>
-              </v-row> </v-col
-          ></v-row>
-        </v-container>
-      </v-main>
-          </v-layout>
-           </v-card>
+        <v-navigation-drawer permanent color="#62ADF4" v-if="$vuetify.display.mdAndUp">
+          <sidebar />
+        </v-navigation-drawer>
+        <navbar v-else />
+        <v-main>
+          <v-container class="fill-height" fluid>
+            <v-row>
+              <v-col cols="12">
+                <v-row class="justify-center align-center">
+                  <v-col cols="12">
+                    <AboutView style="width: 90vw" />
+                  </v-col>
+                </v-row> </v-col
+            ></v-row>
+          </v-container>
+        </v-main>
+      </v-layout>
+    </v-card>
   </v-app>
 </template>
 <script>
 import AboutView from './views/AboutView.vue'
 import sidebar from './components/sidebar.vue'
-
+import navbar from './components/navbar.vue'
 export default {
-  components: { AboutView, sidebar }
+  components: { AboutView, sidebar, navbar }
 }
 </script>
 <style>
-#app{
+#app {
   margin: 0px !important;
   padding: 0px !important;
 }
