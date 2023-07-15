3f19be059c3fa0ca5d54ef7d624cd21e46da9afe
add11e2 <akshaydevadiga4@gmail.com>
2023-07-16 03:35:28 +0530

    Update gtm app instance syntax

--- src/main.js
+++ src/main.js
@@ -27,8 +27,7 @@ const vuetify = createVuetify({
   },
 })
 
-createApp(App).use(router).use(createPinia()).use(vuetify).mount('#app')
-app.use(
+createApp(App).use(router).use(createPinia()).use(vuetify).use(
   createGtm({
     id: 'GTM-WJW4P22', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
     defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
@@ -39,4 +38,4 @@ app.use(
     loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
     vueRouter: router, // Pass the router instance to automatically sync with router (optional)
   }),
-);
+).mount('#app')
